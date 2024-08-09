a8f49596a6cca258ee33d31db24e4c6a7f7b1d19
Nontinium <93352329+Nontinium@users.noreply.github.com>
2024-08-09 21:40:05 +0300

    Add files via upload

---
+++ completetest.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+with open("graphviewersaas\my_final_applicationtest_1.0.0.exe", "rb") as file:
+    btn = st.download_button(
+        label="Download program",
+        data=file,
+        file_name="my_final_applicationtest_1.0.0.exe",
+        mime="image/png",
+    )
